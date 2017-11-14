1b582e95e68fb0495d4539732702a1af2cd14c92
Arthur Douillard <douill_a@epita.fr>
2017-11-14 21:11:51 +0100

    hello: Add hello world with mpi4py .

---
+++ hello.py
@@ -0,0 +1,11 @@
+from mpi4py import MPI
+
+def hello():
+    rank = MPI.COMM_WORLD.Get_rank()
+    size = MPI.COMM_WORLD.Get_size()
+
+    print('Hello World! from rank {}/{}'.format(rank, size))
+
+
+if __name__ == '__main__':
+    hello()
